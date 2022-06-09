f2cf9dfc82dc54a31af84410c2f8945a7de7b230
jcollinc <92186183+jcollinc@users.noreply.github.com>
2022-06-09 12:22:48 -0400

    Time: 76 ms (69.89%), Space: 42.7 MB (82.39%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0
+    let right = numbers.length - 1
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[left] + numbers[right] > target) right--
+        else if (numbers[left] + numbers[right] < target) left++
+        else return [left + 1, right + 1]
+    }
+    
+};
