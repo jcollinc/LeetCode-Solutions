310ba77453d7e489b77e46c97a013e1a07102695
jcollinc <92186183+jcollinc@users.noreply.github.com>
2022-06-17 14:30:45 -0400

    Time: 180 ms (8.33%), Space: 44.8 MB (16.67%) - LeetHub

--- 1196-how-many-apples-can-you-put-into-the-basket/1196-how-many-apples-can-you-put-into-the-basket.js
+++ 1196-how-many-apples-can-you-put-into-the-basket/1196-how-many-apples-can-you-put-into-the-basket.js
@@ -4,10 +4,10 @@
  */
 var maxNumberOfApples = function(weight) {
     if (weight.reduce((a, b) => a + b) <= 5000) return weight.length
-    let res = 0
     let total = 0
+    let res = 0
     weight.sort((a, b) => a - b)
-    for (w of weight) {
+    for (let w of weight) {
         if (total + w <= 5000) {
             total += w
             res++
