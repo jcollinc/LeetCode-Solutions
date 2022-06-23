f63e515f69fbbc46cc425c9a56e095f6897e5fd1
jcollinc <92186183+jcollinc@users.noreply.github.com>
2022-06-23 14:11:37 -0400

    Time: 555 ms (54.64%), Space: 110.2 MB (40.72%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
@@ -1,37 +1,45 @@
-class RandomizedSet {
-    
-  constructor() {
-    this.map = new Map();
-    this.list = [];
-  }
 
-  insert(val) {
-    if (this.map.has(val)) return false;
-    this.map.set(val, this.list.length);
-    this.list.push(val);
-    return true;
-  }
+var RandomizedSet = function() {
+    this.map =  new Map()
+    this.list = []
+    this.swap = (a, b) => {[this.list[a], this.list[b]] = [this.list[b], this.list[a]]}
+};
 
-  remove(val) {
-    if (!this.map.has(val)) return false;
-    const idx = this.map.get(val);
-    this._swap(idx, this.list.length - 1);
-    this.list.pop();
-    this.map.set(this.list[idx], idx);
-    this.map.delete(val);
-    return true;
-  }
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if (this.map.has(val)) return false
+    this.map.set(val, this.list.length)
+    this.list.push(val)
+    return true
+};
 
-  getRandom() {
-    return this.list[Math.floor(Math.random() * this.list.length)];
-  }
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    if (!this.map.has(val)) return false
+    let idx = this.map.get(val)
+    this.swap(idx, this.list.length - 1)
+    this.list.pop()
+    this.map.set(this.list[idx], idx)
+    this.map.delete(val)
+    return true
+};
 
-  _swap(a, b) {
-    const tmp = this.list[a];
-    this.list[a] = this.list[b];
-    this.list[b] = tmp;
-  }
-}
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    return this.list[Math.floor(Math.random() * this.list.length)]
+};
+
+
+
+
 
 /** 
  * Your RandomizedSet object will be instantiated and called as such:
