5815b1c082efdcf11fbf56c2c8324edcfa5c7921
jcollinc <92186183+jcollinc@users.noreply.github.com>
2022-07-06 10:16:48 -0400

    Time: 88 ms (62.75%), Space: 41.4 MB (93.02%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -7,9 +7,9 @@ var fib = function(n) {
     
     let fib = [0, 1]
     
-    for (let i = 2; fib.length < n; i++) {
+    for (let i = 2; fib.length <= n; i++) {
         fib.push(fib[i - 1] + fib[i - 2])        
     }
     
-    return fib[fib.length - 1] + fib[fib.length - 2]
+    return fib.pop()
 };
