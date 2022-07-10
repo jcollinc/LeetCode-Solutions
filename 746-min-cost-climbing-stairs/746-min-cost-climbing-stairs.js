112747bff8047a2d09ce4add515a3f8eb152664c
jcollinc <92186183+jcollinc@users.noreply.github.com>
2022-07-10 10:08:01 -0400

    Time: 114 ms (24.50%), Space: 43 MB (67.87%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,15 +3,9 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    let minimumCost = []
-    for (let i = 0; i <= cost.length; i++) {
-        minimumCost.push(0)
-    }
+    let minimumCost = Array(cost.length + 1).fill(0)
     for (let i = 2; i < minimumCost.length; i++) {
         minimumCost[i] = Math.min(minimumCost[i-1] + cost[i-1], minimumCost[i-2] + cost[i-2])
     }
-    console.log(minimumCost)
     return minimumCost.pop()
 };
-
-[10,15,20, 50, 55, 43, 23, 21, 31]
