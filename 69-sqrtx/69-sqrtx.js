374a1460f3cb5646b0e8a5df44cb33a3b2b02b83
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-10-05 17:33:56 -0400

    Time: 188 ms (11.26%), Space: 43.9 MB (43.48%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x <= 1) return x
+    else {
+        for (let i = 0; i < x; i++) {
+            if (i*i <= x && ((i+1) * (i+1)) > x) return i
+        }
+    }
+};
