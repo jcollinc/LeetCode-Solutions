863cb32cf682e990f64ff92b76e7bfb7652a94a5
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-11-24 18:47:20 -0500

    Time: 71 ms (95.66%), Space: 45.5 MB (5.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,13 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let map = {}
-    for (let i = 0; i < nums.length; i++) {
-        map[nums[i]] = i
-    }
-    for (let i = 0; i < nums.length; i++) {
-        let val = map[target - nums[i]]
-        if (val && val != i) return [val, i]
-    }
-};
+function twoSum(nums, target) {
+  let ref = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    ref.set(nums[i], i) 
+  }
+  for (let i = 0; i < nums.length; i++) {
+    let check = target - nums[i]
+    if (ref.get(check) && ref.get(check) !== i) return [ref.get(check), i]
+  }
+}
