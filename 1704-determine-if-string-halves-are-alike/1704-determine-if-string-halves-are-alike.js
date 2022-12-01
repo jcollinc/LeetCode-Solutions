10d39b0524694d2a26d85deceee4f3a4bf09ebb8
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-11-30 19:42:26 -0500

    Time: 108 ms (47.66%), Space: 43.2 MB (53.91%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -4,10 +4,11 @@
  */
 var halvesAreAlike = function(s) {
     let vowels = {a:1, e:1, i:1, o:1, u:1, A:1, E:1, I:1, O:1, U:1}
+    let mid = s.length / 2
     let count1 = 0
     let count2 = 0
     for (let i = 0; i < s.length; i++) {
-        if (vowels[s[i]] && i < s.length / 2) count1++
+        if (vowels[s[i]] && i < mid) count1++
         else if (vowels[s[i]]) count2++
     }
     return count1 === count2
