81d158ac4f49c986f38d1da6413504673c0371f7
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-14 13:43:52 -0500

    Time: 106 ms (87.28%), Space: 46.5 MB (83.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,19 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    let res = 0
-    let longestLen = 0
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set();
+    let left = 0;
+    let maxSize = 0;
+
+    if (s.length === 0) return 0;
+    if (s.length === 1) return 1;
+
     for (let i = 0; i < s.length; i++) {
-        let j = i+1, counter = 1, ref = {[s[i]]: `${i}`}
-        let left = s[i], right = s[j]
-        while (!ref[s[j]] && j < s.length) {
-            ref[s[j]] = `${i}`
-            counter++
-            j++
+
+        while (set.has(s[i])) {
+            set.delete(s[left])
+            left++;
         }
-        i = parseInt(ref[s[j]])
-        if (counter > res) res = counter
+        set.add(s[i]);
+        maxSize = Math.max(maxSize, i - left + 1)
     }
-    return res
-};
+    return maxSize;
+}
