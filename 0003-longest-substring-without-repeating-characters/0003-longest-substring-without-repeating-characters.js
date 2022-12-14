1213bc7019a6b9479a9b1ea30d199d33f1c0366e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-14 13:32:49 -0500

    Time: 668 ms (14.21%), Space: 69.3 MB (5.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -6,12 +6,14 @@ var lengthOfLongestSubstring = function(s) {
     let res = 0
     let longestLen = 0
     for (let i = 0; i < s.length; i++) {
-        let j = i+1, counter = 1, ref = {[s[i]]: 1}
+        let j = i+1, counter = 1, ref = {[s[i]]: `${i}`}
+        let left = s[i], right = s[j]
         while (!ref[s[j]] && j < s.length) {
-            ref[s[j]] = 1
+            ref[s[j]] = `${i}`
             counter++
             j++
         }
+        i = parseInt(ref[s[j]])
         if (counter > res) res = counter
     }
     return res
