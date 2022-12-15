1829168ec4bbde25823d50737128f733b22ed572
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-14 23:45:13 -0500

    Time: 87 ms (95.71%), Space: 46.8 MB (74.30%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,22 +2,31 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
-    let set = new Set();
-    let left = 0;
-    let maxSize = 0;
 
-    if (s.length === 0) return 0;
-    if (s.length === 1) return 1;
-
-    for (let i = 0; i < s.length; i++) {
-        if(set.has(s[i]))
-        while (set.has(s[i])) {
+/*
+    input = string
+    return = integer
+    
+    initialize variable to track maximum length so far 
+    initialize variable to hold left index of sliding window
+    use for loop with i to represent right index
+    for each i, check if length exceeds current longest
+    if a match is encountered, continue incrementing left index until there is no longer a match
+    
+    
+*/
+var lengthOfLongestSubstring = function(s) {
+    if (s.length === 1) return 1
+    let left = 0
+    let longestLength = 0
+    let set = new Set()
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
             set.delete(s[left])
-            left++;
+            left++ 
         }
-        set.add(s[i]);
-        maxSize = Math.max(maxSize, i - left + 1)
+        set.add(s[right])
+        longestLength = Math.max(longestLength, (right - left + 1))
     }
-    return maxSize;
-}
+    return longestLength
+};
