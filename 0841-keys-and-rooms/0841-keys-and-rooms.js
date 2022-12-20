6236fa603bc20ed5256c1051005e50823717e345
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-20 11:18:11 -0500

    Time: 107 ms (40.18%), Space: 44.7 MB (27.68%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.js
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -5,17 +5,18 @@
 var canVisitAllRooms = function(rooms) {
     const uniqueKeys = new Set([0]);
     const availableKeys = [0];
-    
+    let count = 1
     while (availableKeys.length) {
         let currentKey = availableKeys.pop();
         for (let key of rooms[currentKey]) {
            if (!uniqueKeys.has(key)) {
                uniqueKeys.add(key);
                availableKeys.push(key);
+               count++
            }
         };
     }
 
-    return uniqueKeys.size === rooms.length;
+    return count === rooms.length;
 };
 
