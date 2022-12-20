b4a5f6a706a4d6d80138b4191194bb4fbbee2fd3
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-20 14:01:57 -0500

    Time: 107 ms, Memory: 48.6 MB - LeetHub

--- moving-average-from-data-stream/moving-average-from-data-stream.js
+++ moving-average-from-data-stream/moving-average-from-data-stream.js
@@ -5,6 +5,7 @@ var MovingAverage = function(size) {
     this.sizeList = new Array()
     this.left = 0
     this.size = size
+    this.sum = 0
 };
 
 /** 
@@ -12,9 +13,11 @@ var MovingAverage = function(size) {
  * @return {number}
  */
 MovingAverage.prototype.next = function(val) {
+    if (this.sizeList[this.left]) this.sum -= this.sizeList[this.left]
+    this.sum += val
     this.sizeList[this.left] = val
     this.left = (this.left + 1) % this.size
-    return this.sizeList.reduce((a, b) => a + b) / this.sizeList.length
+    return this.sum / this.sizeList.length
 };
 
 /** 
