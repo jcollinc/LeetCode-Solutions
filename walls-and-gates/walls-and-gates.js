cf251d5da5d6de1b5517696417ed1b7833715f75
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-21 14:04:49 -0500

    Time: 272 ms, Memory: 49.9 MB - LeetHub

--- walls-and-gates/walls-and-gates.js
+++ walls-and-gates/walls-and-gates.js
@@ -2,33 +2,26 @@
  * @param {number[][]} rooms
  * @return {void} Do not return anything, modify rooms in-place instead.
  */
-var wallsAndGates = function(rooms) {   
-    let directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-    let empty = 2147483647, gate = 0
-    
-    const validAddress = (arr, i, j) => {
-        return (i >= 0 && j >= 0 && i < arr.length && j < arr[0].length)
+var wallsAndGates = function(rooms) {
+    let gate = 0, empty = 2147483647 
+    let directions = [[0,1],[0,-1],[1,0],[-1,0]]
+    let addressIsValid = (arr, r, c) => {
+        return r >= 0 && c >= 0 && r < arr.length && c < arr[r].length
     }
-    
-    if(!rooms || rooms.length === 0) return;
     let m = rooms.length, n = rooms[0].length
     let queue = []
-    
     for (let row = 0; row < m; row++) {
         for (let col = 0; col < n; col++) {
-            if (rooms[row][col] === gate) queue.push([row, col])
+            if (rooms[row][col] === gate) queue.push([row, col])      
         }
     }
-    
-    while(queue.length) {
+    while (queue.length) {
         let [gateRow, gateCol] = queue.shift()
-        
-        for (let [x, y] of directions) {
-            let nextRow = gateRow + x, nextCol = gateCol + y
-            if (!validAddress(rooms, nextRow, nextCol) || rooms[nextRow][nextCol] !== empty) continue
-            
-            rooms[nextRow][nextCol] = rooms[gateRow][gateCol] + 1
-            queue.push([nextRow, nextCol])
+        for (let [dx, dy] of directions) {
+            let r = gateRow + dx, c = gateCol + dy
+            if (!addressIsValid(rooms, r, c) || rooms[r][c] !== empty) continue
+            rooms[r][c] = rooms[gateRow][gateCol] + 1
+            queue.push([r, c])
         }
     }
 };
