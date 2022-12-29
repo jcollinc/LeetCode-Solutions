e4420ee8aab2b1e725c1c60640a48fc54466fa98
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-28 20:03:31 -0500

    Time: 67 ms (90.91%), Space: 44.8 MB (74.60%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0, right = nums.length - 1, mid = Math.floor((right + left) / 2)
-    while (nums[mid] !== target && left < right) {    
-        if (nums[mid] === target) return mid
-        else if (nums[mid] > target) {
+    let left = 0, right = nums.length - 1
+    while (left <= right) {    
+        let mid = Math.floor((right + left) / 2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
             right = mid - 1
         } else {
             left = mid + 1
-        }
-        mid = Math.floor((right + left) / 2)
+        }  
     }
-    return nums[mid] === target ? mid : -1
+    return -1
 };
