57e1435769ce80872a728090860f2c779cc385de
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-30 10:14:34 -0500

    Time: 90 ms (82.91%), Space: 51.8 MB (82.39%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,8 +13,15 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if (!root || root === p || root === q) return root
-    let leftSearch = lowestCommonAncestor(root.left, p, q)
-    let rightSearch = lowestCommonAncestor(root.right, p, q)   
-    return leftSearch && rightSearch ? root : (leftSearch || rightSearch) 
+    let parentVal = root.val
+    let pVal = p.val
+    let qVal = q.val
+    
+    if (pVal > parentVal && qVal > parentVal) {
+        return lowestCommonAncestor(root.right, p, q)
+    } else if (pVal < parentVal && qVal < parentVal) {
+        return lowestCommonAncestor(root.left, p, q)
+    } else {
+        return root
+    }
 };
