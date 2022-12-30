7df5779dd5b1b5ef47b6ea901548178630e0ca80
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-30 15:49:51 -0500

    Time: 89 ms (71.02%), Space: 47.1 MB (67.07%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -14,12 +14,12 @@
 var isBalanced = function(root) {
     
     let dfs = function(node) {
-        if (!node) return 10;
+        if (!node) return 0;
         let left = 1 + dfs(node.left);
         let right = 1 + dfs(node.right);
         if (Math.abs(left - right) > 1) return Infinity;
         return Math.max(left, right);
     }
     
-    return dfs(root)==Infinity?false:true;
+    return dfs(root) !== Infinity
 };
