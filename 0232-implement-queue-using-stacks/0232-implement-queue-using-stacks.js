8e4b846efcec9bc5527b4775927d226a5214242e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2022-12-30 22:38:29 -0500

    Time: 54 ms (96.45%), Space: 41.9 MB (56.60%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -1,6 +1,7 @@
 
 var MyQueue = function() {
-    this.queue = []
+    this.stack1 = []
+    this.stack2 = []
 };
 
 /** 
@@ -8,28 +9,42 @@ var MyQueue = function() {
  * @return {void}
  */
 MyQueue.prototype.push = function(x) {
-    this.queue.push(x)
+    this.stack1.push(x)
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.pop = function() {
-    return this.queue.shift()
+    while (this.stack1.length) {
+        this.stack2.push(this.stack1.pop())
+    }
+    let pop = this.stack2.pop()
+    while (this.stack2.length) {
+        this.stack1.push(this.stack2.pop())
+    }
+    return pop
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.peek = function() {
-    return this.queue[0]
+    while (this.stack1.length) {
+        this.stack2.push(this.stack1.pop())
+    }
+    let peek = this.stack2[this.stack2.length-1]
+    while (this.stack2.length) {
+        this.stack1.push(this.stack2.pop())
+    }
+    return peek
 };
 
 /**
  * @return {boolean}
  */
 MyQueue.prototype.empty = function() {
-    return !this.queue.length
+    return !this.stack1.length
 };
 
 /** 
