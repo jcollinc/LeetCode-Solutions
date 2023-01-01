606bc5caff3e1b24c24be0f52ec24c1681fe4e91
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-01 14:39:24 -0500

    Time: 168 ms (88.97%), Space: 68.3 MB (87.02%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    if (!head.next) return true
+    
+    let listLength = 0
+    let seenVals = []
+    
+    let current = head
+    while (current) {
+        current = current.next
+        listLength++
+    }
+    
+    current = head
+    let mid = Math.floor(listLength / 2)
+    let isEven = listLength % 2 === 0
+    while (current) {
+        if (listLength > mid && isEven || listLength - 1 > mid && !isEven) {
+            seenVals.push(current.val)
+            listLength--
+        } else if (listLength < mid || (listLength === mid)) {
+            if (seenVals.pop() !== current.val) return false  
+        } else listLength--
+        current = current.next
+    }
+    return true
+};
