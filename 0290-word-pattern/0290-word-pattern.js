63861b73e953ee39a35874a2c4e2fdfcd3613931
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-01 15:07:11 -0500

    Time: 62 ms (87.45%), Space: 41.9 MB (68.67%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -8,13 +8,11 @@ var wordPattern = function(pattern, s) {
     let mapB = {}
     let words = s.split(" ")
     if (pattern.length !== words.length) return false
-    
     for (let i = 0; i < words.length; i++) {
         if (mapA[pattern[i]] && mapA[pattern[i]] !== words[i] ||
             mapB[words[i]] && mapB[words[i]] !== pattern[i]) return false
         mapA[pattern[i]] = words[i]
         mapB[words[i]] = pattern[i]
     }
-    
     return true
 };
