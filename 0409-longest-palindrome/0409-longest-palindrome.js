6066f13a70ce3b1ca5a56f91bbdc9b183fdb37cb
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-01 13:36:52 -0500

    Time: 67 ms (93.44%), Space: 42.7 MB (92.21%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -6,14 +6,9 @@ var longestPalindrome = function(s) {
     if (s.length === 1) return 1
     let longest = 0
     let stringMap = {}
-    let oneCount = false
     for (let char of s) {
         stringMap[char] = stringMap[char] + 1 || 1
+        if (stringMap[char] % 2 === 0) longest += 2
     }
-    for (let count of Object.values(stringMap)) {
-        if (count % 2 === 0) longest += count
-        else longest += count - 1, oneCount = true
-    }
-    if (oneCount) longest += 1
-    return longest
+    return longest < s.length ? longest + 1 : longest
 };
