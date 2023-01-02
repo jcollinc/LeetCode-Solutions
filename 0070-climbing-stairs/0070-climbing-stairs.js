72a4c281dfdca4b3c784aa9e3ad4c12b28cbddb9
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-02 12:32:49 -0500

    Time: 110 ms (7.35%), Space: 41.7 MB (77.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let dp = new Array(n + 1);
-    dp[1] = 1, dp[2] = 2;
-    for (let i = 3; i <= n; i++) {
-        dp[i] = dp[i-1] + dp[i-2];
+    let steps = [0, 1, 2, 3] 
+    if (n <= 3) return n
+    for (let i = 4; i <= n; i++) {
+        steps[i] = steps[i-1] + steps[i-2]
     }
-    return dp[n];
+    return steps[n]
 };
