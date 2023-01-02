7b5ede59e94e5ab29eeeaf95d766c5582eef8455
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-02 12:51:53 -0500

    Time: 86 ms (47.05%), Space: 41.9 MB (40.82%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -4,8 +4,11 @@
  */
 var tribonacci = function(n) {
     let tribVals = [0, 1, 1]
-    for (let i = 3; i <=n; i++) {
-        tribVals[i] = tribVals[i-3] + tribVals[i-2] + tribVals[i-1]
+    for (let i = 3; i <= n; i++) {
+        let newVal = (tribVals[0] + tribVals[1] + tribVals[2])
+        tribVals[0] = tribVals[1]
+        tribVals[1] = tribVals[2]
+        tribVals[2] = newVal  
     }
-    return tribVals[n]
+    return n > 2 ? tribVals[2] : tribVals[n]
 };
