31197c8c676702f4146afb6c49c8f3c2b4d55f64
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-02 10:04:01 -0500

    Time: 67 ms (88.83%), Space: 42.3 MB (60.37%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.js
+++ 0520-detect-capital/0520-detect-capital.js
@@ -3,5 +3,5 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-    return (word === word.toUpperCase() || word === word.toLowerCase() || word === word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase())
+    return (word === word.toUpperCase() || word === word.toLowerCase() || word === word[0].toUpperCase() + word.slice(1).toLowerCase())
 };
