b66a71d5a36749e07cb7fe93de13b32e2731b273
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-04 11:04:54 -0500

    Time: 64 ms (94.21%), Space: 44.7 MB (18.43%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,20 +4,19 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    a = a.split("").reverse().join("");
-    b = b.split("").reverse().join("");
-    len = a.length > b.length ? a.length : b.length;
-    result = [];
-    for(let i = 0; i < len; i += 1){
-        num1 = Number(a[i] || 0);
-        num2 = Number(b[i]) || 0;
-        curr = Number(result[i]||0) + num1 + num2
-        if(curr >= 2){
-            result[i] = curr%2;
+    a = a.split("").reverse().join("")
+    b = b.split("").reverse().join("")
+    let len = Math.max(a.length, b.length)
+    let result = []
+    for (let i = 0; i < len; i++) {
+        num1 = Number(a[i] || 0)
+        num2 = Number(b[i] || 0)
+        sum = (result[i] || 0) + num1 + num2
+        if (sum >= 2) {
+            result[i] = sum % 2
             result.push(1)
-        }
-        else{
-            result[i] = curr
+        } else {
+            result[i] = sum
         }
     }
     return result.reverse().join("")
