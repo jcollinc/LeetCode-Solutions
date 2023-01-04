b32b7117fc669390b72b5cb0472c2810d456023e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-04 14:07:31 -0500

    Time: 114 ms (33.87%), Space: 44.1 MB (59.12%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.js
+++ 0252-meeting-rooms/0252-meeting-rooms.js
@@ -5,11 +5,8 @@
 var canAttendMeetings = function(intervals) {
     if (!intervals.length) return true
     intervals.sort((a, b) => a[0] - b[0])
-    console.log(intervals)
-    let end = intervals[0][1]
-    for (let meetingIdx = 1; meetingIdx < intervals.length; meetingIdx++) {
-        if (intervals[meetingIdx][0] < end) return false
-        end = intervals[meetingIdx][1]
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] < intervals[i-1][1]) return false
     }
     return true
 };
