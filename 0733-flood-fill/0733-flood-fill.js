34495e96b71e9fd6d409ecbc4c8601a8329a97d1
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-09 13:46:18 -0500

    Time: 83 ms (75.22%), Space: 44.6 MB (24.82%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -5,19 +5,15 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, newColor, toChange = image[sr][sc]) {
-    if (sr < 0 || 
-        sc < 0 || 
-        sr >= image.length || 
-        sc >= image[sr].length || 
-        image[sr][sc] !== toChange || 
-        image[sr][sc] === newColor) {
-        return image
+var floodFill = function(image, sr, sc, color, toChange = image[sr][sc]) {
+    function outOfBounds() {
+        return (sr < 0 || sc < 0 || sr >= image.length || sc >= image[sr].length)
     }
+    if (outOfBounds() || toChange === color || image[sr][sc] !== toChange) return image
     let directions = [[0,1],[0,-1],[1,0],[-1,0]]
-    image[sr][sc] = newColor
-    for (let [dy, dx] of directions) {
-        floodFill(image, sr+dy, sc + dx, newColor, toChange)
+    image[sr][sc] = color
+    for (let [dx,dy] of directions) {
+        floodFill(image, sr+dx, sc+dy, color, toChange)
     }
     return image
 };
