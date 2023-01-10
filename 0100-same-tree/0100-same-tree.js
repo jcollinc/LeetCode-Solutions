c9db7f4f1f46fb9a6a9beac8fd5883a5d671b545
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-10 09:56:30 -0500

    Time: 79 ms (55.58%), Space: 41.8 MB (96.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,8 +12,8 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if (!p && !q) return true
     if (p && !q || q && !p) return false
+    if (!p && !q) return true
     if (p.val !== q.val) return false
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
