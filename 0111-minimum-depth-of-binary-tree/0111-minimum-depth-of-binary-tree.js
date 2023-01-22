70978161b4aee8f65570c942a8710f4fb60daf8e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-22 10:17:36 -0500

    Time: 299 ms (38.75%), Space: 100.6 MB (5.88%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -10,11 +10,9 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var minDepth = function(root) {
-    if (!root) return 0
-    if (!root.left && !root.right) return 1
-    let min = Infinity
-    if (root.left) min = Math.min(minDepth(root.left), min)
-    if (root.right) min = Math.min(minDepth(root.right), min)
-    return min + 1
+var minDepth = function(root, min = 1) {
+    if (!root && min === 1) return 0
+    if (!root) return Infinity
+    if (!root.left && !root.right) return min
+    return Math.min(minDepth(root.left, min + 1), minDepth(root.right, min + 1))
 };
