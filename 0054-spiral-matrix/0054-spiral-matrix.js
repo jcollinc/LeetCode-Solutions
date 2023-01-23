b5b19e5af66d2fc21b6b57b59a74841487d116fe
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-01-23 15:50:33 -0500

    Time: 99 ms (13.70%), Space: 41.6 MB (75.34%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -3,25 +3,26 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    if (matrix.length == 0) return [];
-    let res = [];
-    let row1 = 0, col1 = 0, row2 = matrix.length-1, col2 = matrix[0].length-1;
-    while (row1 <= row2 && col1 <= col2) {
-        for (let col = col1; col <= col2; col++) {
-            res.push(matrix[row1][col]);    
+    let top = 0, right = matrix[0].length - 1, bottom = matrix.length - 1, left = 0
+    let res = [], size = matrix.length * matrix[0].length
+    let sizeCheck = () => res.length < size
+    while (res.length < size) {
+        for (let col = left; col <= right && sizeCheck(); col++) {
+            res.push(matrix[top][col])
         }
-        for (let row = row1+1; row <= row2; row++) {
-            res.push(matrix[row][col2]);
+        top++
+        for (let row = top; row <= bottom && sizeCheck(); row++) {
+            res.push(matrix[row][right])
         }
-        if (row1 < row2 && col1 < col2) {
-            for (let col = col2-1; col >= col1; col--) {
-                res.push(matrix[row2][col]);
-            }
-            for (let row = row2-1; row > row1; row--) {
-                res.push(matrix[row][col1]);
-            }    
+        right--
+        for (let col = right; col >= left && sizeCheck(); col--) {
+            res.push(matrix[bottom][col])
         }
-        row1++, col1++, row2--, col2--;
+        bottom--
+        for (let row = bottom; row >= top && sizeCheck(); row--) {
+            res.push(matrix[row][left])
+        }
+        left++
     }
-    return res;
+    return res
 };
