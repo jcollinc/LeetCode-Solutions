b01b1e5c9db12626e3786f42fa1449f90c25337a
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-09 16:05:16 -0500

    Time: 8596 ms (5.36%), Space: 75.7 MB (83.80%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    let output = []
+    for (let i = 0; i + k - 1 < nums.length; i++) {
+        let temp = i, left = i, right = i + k - 1, max = -Infinity
+        while (temp <= right) {
+            max = Math.max(max, nums[temp])
+            temp++
+        }
+        output.push(max)
+        if (nums[i] !== max && right !== nums.length - 1) {
+            output.push(Math.max(max, (nums[right+1] || -Infinity)))
+            i++
+        }
+    }
+    return output
+};
