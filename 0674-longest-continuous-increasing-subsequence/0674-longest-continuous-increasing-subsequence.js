de82e05ae93ad42bf0087a56dfcb33b90e6b4c9c
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-09 17:34:10 -0500

    Time: 61 ms (91.09%), Space: 42.3 MB (73.27%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.js
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.js
@@ -5,11 +5,11 @@
 var findLengthOfLCIS = function(nums) {
     let maxLength = 0
     for (let i = 0; i < nums.length;) {
-        let start = i, end = i + 1
+        let end = i + 1
         while (nums[end] > nums[end - 1] && end < nums.length) {
             end++
         }
-        maxLength = Math.max(maxLength, end - start)
+        maxLength = Math.max(maxLength, end - i)
         i = end
     }
     return maxLength
