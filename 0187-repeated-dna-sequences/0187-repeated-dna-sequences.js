7491b25fc2537ec779db5ab76687d8799dd39459
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-10 17:25:20 -0500

    Time: 97 ms (73.81%), Space: 55 MB (57.86%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
@@ -2,10 +2,10 @@
  * @param {string} s
  * @return {string[]}
  */
-var findRepeatedDnaSequences = function(s) {
-    if (10 >= s.length) return []
+var findRepeatedDnaSequences = function(s, k = 10) {
+    if (k >= s.length) return []
     let output = [], seenSequences = new Map()
-    let left = 0, right = 10
+    let left = 0, right = k
     while (right <= s.length) {
         let sequence = s.slice(left, right), count = seenSequences.get(sequence)
         if (seenSequences.has(sequence) && count && count === 1) output.push(sequence)
