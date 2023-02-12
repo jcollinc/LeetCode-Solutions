09cb26cf18ed9616d172fa26294d06b920cdd517
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-12 10:25:01 -0500

    Time: 62 ms (95.34%), Space: 43.4 MB (73.51%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    if (nums.length === 1) return nums[0]
-    nums.sort((a,b) => a - b) 
-    for (let i = 0; i < nums.length - 1;) {
-        if (nums[i] !== nums[i+1]) return nums[i]
-        i += 2
-    }
-    return nums[nums.length - 1]
+    return nums.reduce((prev, curr) => prev ^ curr);
 };
