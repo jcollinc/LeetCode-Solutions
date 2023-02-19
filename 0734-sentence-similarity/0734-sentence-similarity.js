53bfcf10617fb0623477059b0385a168bb009858
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-19 16:27:29 -0500

    Time: 64 ms (83.92%), Space: 44.4 MB (33.17%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.js
+++ 0734-sentence-similarity/0734-sentence-similarity.js
@@ -5,16 +5,14 @@
  * @return {boolean}
  */
 var areSentencesSimilar = function(sentence1, sentence2, similarPairs) {
-    let len1 = sentence1.length, len2 = sentence2.length
-    if (len1 !== len2) return false
+    if (sentence1.length !== sentence2.length) return false
     let pairsSet = new Set()
     for (let pair of similarPairs) {
         pairsSet.add(`${pair[0]}, ${pair[1]}`)
-        pairsSet.add(`${pair[1]}, ${pair[0]}`)
     }
-    for (let i = 0; i < len1; i++) {
+    for (let i = 0; i < sentence1.length; i++) {
         let [str1, str2] = [sentence1[i], sentence2[i]]
-        if (!pairsSet.has(`${str1}, ${str2}`) && str1 !== str2) return false 
+        if (!pairsSet.has(`${str1}, ${str2}`) && !pairsSet.has(`${str2}, ${str1}`) && str1 !== str2) return false 
     }
     return true
 };
