dfc6a36878f3ab7385f53cce291219df0727d3c6
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-23 09:23:20 -0500

    Time: 109 ms (92.77%), Space: 55.2 MB (91.57%) - LeetHub

--- 0502-ipo/0502-ipo.js
+++ 0502-ipo/0502-ipo.js
@@ -6,27 +6,47 @@
  * @return {number}
  */
 var findMaximizedCapital = function(k, w, profits, capital) {
-   if(w >= Math.max(...capital)) {
+    if (w >= Math.max(...capital)) {
       profits.sort((a, b) => b - a);
       return profits.slice(0, k).reduce((acc, num) => acc + num, w);
-   }
-    
-    for (let i = 0; i < k; i++) {
-       let maxProfit = -Infinity;
-       let projectIndex = -1;
-       for (let j = 0; j < profits.length; j++) {
-          if (w < capital[j]) continue;
-          
-          if (profits[j] >= maxProfit) {
-            projectIndex = j;
-            maxProfit = profits[j];
-          }
-       }
-       if (projectIndex === -1) {
-         break;
-       }
-       capital[projectIndex] = Infinity;
-       w += maxProfit;        
     }
-    return w;
+    let projectsCompleted = 0, totalCapital = w
+    while (projectsCompleted < k) {
+        let maxProfit = 0, n = profits.length, currentCompletedProject = -1
+        for (let i = 0; i < n; i++) {
+            if (capital[i] > totalCapital) continue
+            if (profits[i] > maxProfit) {
+                maxProfit = profits[i]
+                currentCompletedProject = i
+            }
+        }
+        if (currentCompletedProject === -1) break
+        projectsCompleted++
+        capital[currentCompletedProject] = Infinity
+        totalCapital += (maxProfit)
+    }
+    return totalCapital
 };
+
+
+
+//     for (let i = 0; i < k; i++) {
+//        let maxProfit = -Infinity;
+//        let projectIndex = -1;
+//        for (let j = 0; j < profits.length; j++) {
+//           if (w < capital[j]) continue;
+          
+//           if (profits[j] >= maxProfit) {
+//             projectIndex = j;
+//             maxProfit = profits[j];
+//           }
+//        }
+//        if (projectIndex === -1) {
+//          break;
+//        }
+//        capital[projectIndex] = Infinity;
+//        w += maxProfit;        
+//     }
+//     return w;
+
+
