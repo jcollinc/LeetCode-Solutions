7ac43265369e9147e970ac407f5c197d591bceaa
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-23 09:24:35 -0500

    Time: 93 ms (97.59%), Space: 55 MB (92.77%) - LeetHub

--- 0502-ipo/0502-ipo.js
+++ 0502-ipo/0502-ipo.js
@@ -23,30 +23,11 @@ var findMaximizedCapital = function(k, w, profits, capital) {
         if (currentCompletedProject === -1) break
         projectsCompleted++
         capital[currentCompletedProject] = Infinity
-        totalCapital += (maxProfit)
+        totalCapital += maxProfit
     }
     return totalCapital
 };
 
 
-
-//     for (let i = 0; i < k; i++) {
-//        let maxProfit = -Infinity;
-//        let projectIndex = -1;
-//        for (let j = 0; j < profits.length; j++) {
-//           if (w < capital[j]) continue;
-          
-//           if (profits[j] >= maxProfit) {
-//             projectIndex = j;
-//             maxProfit = profits[j];
-//           }
-//        }
-//        if (projectIndex === -1) {
-//          break;
-//        }
-//        capital[projectIndex] = Infinity;
-//        w += maxProfit;        
-//     }
-//     return w;
 
 
