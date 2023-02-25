fcfcdbd059fc72553e5550b41a221c6d9dc64e2e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-25 09:04:51 -0500

    Time: 92 ms (75.35%), Space: 49.6 MB (54.57%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,37 +12,12 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let lenA = 0, lenB = 0, nodeA = headA, nodeB = headB
+    pA = headA, pB = headB
     
-    while (nodeA) {
-        lenA++
-        nodeA = nodeA.next
-    }
-    while (nodeB) {
-        lenB++
-        nodeB = nodeB.next
+    while (pA !== pB) {
+        pA = !pA ? headB : pA.next
+        pB = !pB ? headA : pB.next 
     }
     
-    nodeA = headA
-    nodeB = headB
-    
-    if (lenA > lenB) {    
-        while (lenA - lenB > 0) {
-            nodeA = nodeA.next
-            lenA--
-        }
-    } else if (lenA < lenB) {
-        while (lenB - lenA > 0) {
-            nodeB = nodeB.next
-            lenB--
-        }   
-    }
-    
-    while (nodeA && nodeB) {
-        if (nodeA === nodeB) return nodeA
-        nodeA = nodeA.next
-        nodeB = nodeB.next
-    }
-    
-    return null
+    return pA
 };
