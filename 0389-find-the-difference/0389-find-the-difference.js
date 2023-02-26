da4988c937845a2166b5213ffb9351ec61314aff
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-26 14:13:55 -0500

    Time: 73 ms (70.10%), Space: 43.2 MB (80.03%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -5,13 +5,16 @@
  */
 var findTheDifference = function(s, t) {
     if (!s.length) return t
-    let sMap = new Map()
-    for (let char of s) {
-        sMap.set(char, 1 + (sMap.get(char) || 0))
+    
+    s = s.split("").sort()
+    t = t.split("").sort()
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== t[i]) {
+            console.log(s[i], t[i])
+            return t[i]
+        }
     }
-    console.log(sMap)
-    for (let char of t) {
-        if (!sMap.has(char) || sMap.get(char) === 0) return char
-        else (sMap.set(char, sMap.get(char) - 1))
-    }
+    
+    return t[t.length - 1]
 };
