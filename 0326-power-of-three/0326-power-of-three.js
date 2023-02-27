baf4a27940d7bda0fc13166dd060e5a318de7ea2
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-02-27 09:01:23 -0500

    Time: 214 ms (68.12%), Space: 51.6 MB (10.56%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.js
+++ 0326-power-of-three/0326-power-of-three.js
@@ -3,11 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if (n <= 0) return false
-    let curr = 0, idx = 0
-    while (curr < n) {
-        curr = 3**idx
-        idx++
-    }
-    return curr === n
+    return n > 0 && 1162261467 % n == 0;
 };
