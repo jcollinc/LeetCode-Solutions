f0ef6ac9e769143eea95a72272a68020a2052dc6
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-03-09 14:19:17 -0500

    Time: 73 ms (87.96%), Space: 45.2 MB (25.33%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let seen = new Set()
+    while (head) {
+        if (seen.has(head)) return head
+        else {
+            seen.add(head)
+            head = head.next
+        }
+    }
+    return null
+};
