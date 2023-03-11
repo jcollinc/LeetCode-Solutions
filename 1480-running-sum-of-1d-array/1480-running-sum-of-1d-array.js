e421da78e8fa77fdc101aa1efd0dba7487ac2172
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-03-10 19:09:16 -0500

    Time: 68 ms (46.11%), Space: 42.4 MB (40.45%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,6 +3,8 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let total = 0
-    return nums.map(num => total += num)
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = nums[i] + nums[i-1]
+    }
+    return nums
 };
