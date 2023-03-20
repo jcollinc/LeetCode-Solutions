cf75376936033cb9b2ff6c421d48400c285c96c9
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-03-20 13:01:52 -0400

    Time: 74 ms (40.27%), Space: 43.8 MB (82.67%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -4,11 +4,13 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
-    for (let i = 0; i < flowerbed.length && n > 0; i++) {
-        if (flowerbed[i] === 0 && !flowerbed[i-1] && !flowerbed[i+1]) {
-            flowerbed[i] = 1
+    let idx = 0
+    while (n > 0 && idx < flowerbed.length) {
+        if (flowerbed[idx] === 0 && !flowerbed[idx-1] && !flowerbed[idx+1]) {
+            flowerbed[idx] = 1
             n--
         }
+        idx++
     }
     return n === 0
 };
