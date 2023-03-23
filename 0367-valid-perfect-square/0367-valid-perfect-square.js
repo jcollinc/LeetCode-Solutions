d3fde837bd01bb8c839b72c7d07f12839eff2f59
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-03-23 19:06:38 -0400

    Time: 53 ms (91.52%), Space: 41.9 MB (46.62%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -4,9 +4,9 @@
  */
 var isPerfectSquare = function(num) {
     let left = 0, right = num
-    while (left !== right) {
+    while (left <= right) {
         let mid = Math.floor((left + right) / 2)
-        if (mid * mid > num) right = mid
+        if (mid * mid > num) right = mid - 1
         else if (mid * mid < num) left = mid + 1
         else return true
     }
