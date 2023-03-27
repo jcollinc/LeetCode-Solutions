727d0320a9f829f7fb0e2bf95c0385bf9ae48857
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-03-27 11:19:34 -0400

    Time: 63 ms (93.63%), Space: 44.7 MB (28.19%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -3,23 +3,16 @@
  * @param {number[]} newInterval
  * @return {number[][]}
  */
-var insert = function (intervals, newInterval) {
-  let [newStart, newEnd] = newInterval;
-  let left = [];
-  let right = [];
-  
-  for (const interval of intervals) {
-    const [currStart, currEnd] = interval;
-	
-    if (currEnd < newStart) left.push(interval);
-      
-    else if (currStart > newEnd) right.push(interval);
-      
-    else {
-      newStart = Math.min(newStart, currStart);
-      newEnd = Math.max(newEnd, currEnd);
+var insert = function(intervals, newInterval) {
+    let left = [], right = [], start = newInterval[0], end = newInterval[1]
+    for (let interval of intervals) {
+        let curr0 = interval[0], curr1 = interval[1]
+        if (curr1 < start) left.push(interval)
+        else if (curr0 > end) right.push(interval)
+        else {
+            start = Math.min(start, curr0)
+            end = Math.max(end, curr1)
+        }
     }
-  }
-  
-  return [...left, [newStart, newEnd], ...right]; 
+    return [...left, [start, end], ...right]
 };
