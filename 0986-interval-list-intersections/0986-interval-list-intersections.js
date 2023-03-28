5d6dcfcd2358137ecc99fe70f9607c394e20a588
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-03-28 11:54:34 -0400

    Time: 93 ms (66.94%), Space: 49.5 MB (23.89%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.js
+++ 0986-interval-list-intersections/0986-interval-list-intersections.js
@@ -3,17 +3,14 @@
  * @param {number[][]} secondList
  * @return {number[][]}
  */
-var intervalIntersection = function(firstList, secondList) {
-    let lenA = firstList.length, lenB = secondList.length
-    let pointerA = 0, pointerB = 0, output = []
-    
-    while (pointerA < lenA && pointerB < lenB) {
-        let currA = firstList[pointerA], currB = secondList[pointerB]
-        let low = Math.max(currA[0], currB[0]), high = Math.min(currA[1], currB[1])
-        if (low <= high) {
-            output.push([low, high])
+var intervalIntersection = function(list1, list2) {
+    let ptr1 = 0, ptr2 = 0, output = []
+    while (ptr1 < list1.length && ptr2 < list2.length) {
+        let [start1, end1] = list1[ptr1], [start2, end2] = list2[ptr2]
+        if (end2 >= start1 && start2 <= end1) {
+            output.push([Math.max(start1, start2), Math.min(end1,end2)])
         }
-        currA[1] < currB[1] ? pointerA++ : pointerB++
+        end1 < end2 ? ptr1++ : ptr2++
     }
     return output
 };
