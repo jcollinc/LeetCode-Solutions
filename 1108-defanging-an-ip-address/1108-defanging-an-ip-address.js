5565edba5d48a3086dbc9657cbb0be71d6680864
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-03-28 15:10:00 -0400

    Time: 58 ms (71.25%), Space: 41.4 MB (91.94%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,9 +3,5 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    let output = ""
-    for (let char of address) {
-        output += char === "." ? "[.]" : char
-    }
-    return output
+    return address.replace(/\./g, '[.]');
 };
