6b192d0b6c60defaa5d62f3fab0e9b299ce300e8
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-04-03 15:38:24 -0400

    Time: 88 ms (63.16%), Space: 50.1 MB (45.61%) - LeetHub

--- 0759-employee-free-time/0759-employee-free-time.js
+++ 0759-employee-free-time/0759-employee-free-time.js
@@ -11,13 +11,7 @@
  * @return {Interval[]}
  */
 var employeeFreeTime = function(schedule) {
-    let allIntervals = []
-    for (let empSchedule of schedule) {
-        for (let interval of empSchedule) {
-            allIntervals.push(interval)
-        }
-    }
-    allIntervals.sort((a,b) => a.start - b.start)
+    let allIntervals = schedule.reduce( (a,b) => [...a, ...b]).sort( (a,b) => a.start - b.start )
     let output = [], nextEnd = allIntervals[0].end
     for (let interval of allIntervals) {
         if (interval.start > nextEnd) output.push(new Interval(nextEnd, interval.start))
