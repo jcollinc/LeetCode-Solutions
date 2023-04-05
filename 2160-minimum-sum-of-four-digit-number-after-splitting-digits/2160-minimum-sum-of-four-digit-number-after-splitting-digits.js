85f2fc683b40f443aff1c7acc7f0ff77c6a112a7
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-04-05 11:36:14 -0400

    Time: 51 ms (93.23%), Space: 41.9 MB (75.38%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -4,6 +4,5 @@
  */
 var minimumSum = function(num) {
     let numArr = num.toString().split("").sort()
-    let num1 = numArr[0] + numArr[2], num2 = numArr[1] + numArr[3]
-    return parseInt(num1) + parseInt(num2)
+    return parseInt(numArr[0] + numArr[2]) + parseInt(numArr[1] + numArr[3])
 };
