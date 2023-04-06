2ce205d1fac99a60f1c1484f01de99d90b3f6e43
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-04-05 22:31:49 -0400

    Time: 55 ms (82.87%), Space: 42.2 MB (28.05%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -3,13 +3,5 @@
  * @return {string}
  */
 var interpret = function(command) {
-    let output = "", char = "", map = new Map([["G", "G"],["()","o"],["(al)", "al"]])
-    for (let i = 0; i < command.length; i++) {
-        char += command[i]
-        if (map.has(char)) {
-            output += map.get(char)
-            char = ""
-        }
-    }
-    return output
+    return command.split("()").join("o").split("(al)").join("al")
 };
