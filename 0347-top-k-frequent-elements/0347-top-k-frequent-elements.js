1e8a5c5b23bfb270513f99e844bb43c47924d40e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-05-22 13:09:52 -0400

    Time: 66 ms (91.95%), Space: 47.1 MB (24.94%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let counts = new Map()
+    for (let num of nums) {
+        counts.set(num, 1 + (counts.get(num) || 0))
+    }
+    let countsArr = []
+    
+    for (let [num, count] of counts) {
+        countsArr.push([num, count])
+    }
+    
+    countsArr.sort((a,b) => b[1] - a[1])
+    
+    countsArr.length = k 
+    
+    return countsArr.map((a) => a[0])
+};
