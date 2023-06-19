0369306d0c34e82cbde98eade48e0ba3531064d4
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-06-19 11:59:13 -0400

    Time: 98 ms (85.57%), Space: 56.6 MB (12.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,18 +4,16 @@
  */
 var productExceptSelf = function (nums) {
   const res = [];
-  let product = 1;
+  let product = 1, n = nums.length
 
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < n; i++) {
     res.push(product);
     product *= nums[i];
   }
 
-  console.log(res)  
-
   product = 1;
 
-  for (let j = nums.length - 1; j >= 0; j--) {
+  for (let j = n - 1; j >= 0; j--) {
     res[j] *= product;
     product *= nums[j];
   }
