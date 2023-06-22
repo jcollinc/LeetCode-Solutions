b9b3926bd647b7af41d5654051d9c0268fde2485
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-06-22 14:13:26 -0400

    Time: 71 ms (95.63%), Space: 49.2 MB (73.79%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function(P, F) {
+    let len = P.length, buying = 0, selling = -P[0]
+    for (let i = 1; i < len; i++) {
+        buying = Math.max(buying, selling + P[i] - F)
+        selling = Math.max(selling, buying - P[i])
+    }
+    return buying
+};
