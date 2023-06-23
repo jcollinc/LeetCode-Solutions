e2eedc61789b91a561d91581fe965746ea97fa94
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-06-23 10:31:56 -0400

    Time: 81 ms (98.56%), Space: 66.8 MB (27.71%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -6,9 +6,8 @@
 var containsNearbyDuplicate = function(nums, k) {
     let map = new Map()
     for (let i = 0; i < nums.length; i++) {
-        let num = nums[i]
-        if (i - map.get(num) <= k) return true
-        map.set(num, i)
+        if (i - map.get(nums[i]) <= k) return true
+        map.set(nums[i], i)
     }
     return false
 };
