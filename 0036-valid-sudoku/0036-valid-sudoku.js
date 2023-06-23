a36a8d16ee28d815ccdabba8b764f5e522ce482b
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-06-23 15:32:28 -0400

    Time: 65 ms (94.73%), Space: 44.2 MB (95.57%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -3,42 +3,30 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    //helper function to check 3x3 grid
-    function checkGrid (row, col) {
-        let seen = new Set()
-        for (let i = row; i < row + 3; i++) {
-            for (let j = col; j < col + 3; j++) {
-                let curr = board[i][j]
-                if (seen.has(curr)) return false
-                else if (curr !== ".") seen.add(curr)
-            }
-        }
-        return true
+  for (let i = 0; i < 9; i++) {
+    let row = new Set(),
+        col = new Set(),
+        box = new Set();
+
+    for (let j = 0; j < 9; j++) {
+      let _row = board[i][j];
+      let _col = board[j][i];
+      let _box = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
+      
+      if (_row != '.') {
+        if (row.has(_row)) return false;
+        row.add(_row);
+      }
+      if (_col != '.') {
+        if (col.has(_col)) return false;
+        col.add(_col);
+      }
+      
+      if (_box != '.') {
+        if (box.has(_box)) return false;
+        box.add(_box);
+      } 
     }
-    //Check rule-break in row
-    for (let row of board) {
-        let seen = new Set()
-        for (let num of row) {
-            if (seen.has(num)) return false
-            else if (num !== ".") seen.add(num)
-        }
-    }
-    //Check rule-break in column
-    for (let col = 0; col < board[0].length; col++) {
-        let seen = new Set()
-        for (let row = 0; row < board.length; row++) {
-            let curr = board[row][col]
-            if (seen.has(curr)) return false
-            else if (curr !== ".") seen.add(curr)
-        }
-    }
-    //Check rule-break in 3x3 grid
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[0].length; col++) {
-            if (row % 3 === 0 && col % 3 === 0) {
-                if (!checkGrid(row, col)) return false
-            }
-        }
-    }
-    return true
+  }
+  return true
 };
