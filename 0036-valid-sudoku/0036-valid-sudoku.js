00b756d5923435112202b3e0b73349efff38220e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-06-23 15:10:06 -0400

    Time: 88 ms (28.97%), Space: 44.5 MB (88.14%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -3,37 +3,42 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    let rows = new Map()
-    let cols = new Map()
-    let grids = new Map()
-    let grid = '00'
-    
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[0].length; col++) {
-            rows.set(row, new Set())
-            cols.set(col, new Set())
-            if (row % 3 === 0 && col % 3 === 0) grids.set(`${row}${col}`, new Set())
+    //helper function to check 3x3 grid
+    function checkGrid (row, col) {
+        let seen = new Set()
+        for (let i = row; i < row + 3; i++) {
+            for (let j = col; j < col + 3; j++) {
+                let curr = board[i][j]
+                if (seen.has(curr)) return false
+                else if (curr !== ".") seen.add(curr)
+            }
+        }
+        return true
+    }
+    //Check rule-break in row
+    for (let row of board) {
+        let seen = new Set()
+        for (let num of row) {
+            if (seen.has(num)) return false
+            else if (num !== ".") seen.add(num)
         }
     }
-
-    for (let row = 0; row < board.length; row++) {
-        if (row % 3 === 0) grid = `${row}` + grid.slice(0, 1);
-        for (let col = 0; col < board[0].length; col++) {
-            if (col % 3 === 0) grid = grid.slice(0, -1) + `${col}`;
+    //Check rule-break in column
+    for (let col = 0; col < board[0].length; col++) {
+        let seen = new Set()
+        for (let row = 0; row < board.length; row++) {
             let curr = board[row][col]
-            if (curr === '.') continue
-            if (
-                rows.get(row).has(curr) || 
-                cols.get(col).has(curr) ||
-                grids.get(grid).has(curr)
-            ) return false
-            else {
-                rows.set(row, rows.get(row).add(curr))
-                cols.set(col, cols.get(col).add(curr))
-                grids.set(grid, grids.get(grid).add(curr))
+            if (seen.has(curr)) return false
+            else if (curr !== ".") seen.add(curr)
+        }
+    }
+    //Check rule-break in 3x3 grid
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[0].length; col++) {
+            if (row % 3 === 0 && col % 3 === 0) {
+                if (!checkGrid(row, col)) return false
             }
         }
     }
-    console.log(rows, cols, grids)
     return true
 };
