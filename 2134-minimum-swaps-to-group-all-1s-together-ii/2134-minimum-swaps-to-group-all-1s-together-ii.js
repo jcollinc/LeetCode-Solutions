008664534740579e8e8179347ea03520479dc8ec
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-06-26 14:46:32 -0400

    Time: 99 ms (58.33%), Space: 51.2 MB (75.00%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.js
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var minSwaps = function(nums) {
-    let oneCount = 0, zeroCount = 0
-    let n = nums.length
+    let oneCount = 0, zeroCount = 0, n = nums.length
     for (let num of nums) {
         if (num === 1) oneCount++
     }
+    if (oneCount === n) return 0
     for (let i = 0; i < oneCount; i++) {
         if (nums[i] === 0) zeroCount++
     }
+
     let start = 0, end = oneCount - 1, minZeros = zeroCount
     
     while (start < n) {
+        if (zeroCount === 0) return 0
         if (nums[start] === 0) zeroCount--
         start++
         end = (end + 1) % n
