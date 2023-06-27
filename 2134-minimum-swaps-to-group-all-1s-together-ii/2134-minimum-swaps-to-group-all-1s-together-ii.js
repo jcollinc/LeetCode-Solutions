e72c705c1dbb556067f038aa08665bfa523fbfa1
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-06-27 09:46:38 -0400

    Time: 96 ms (65.00%), Space: 51.3 MB (65.00%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.js
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.js
@@ -3,20 +3,28 @@
  * @return {number}
  */
 var minSwaps = function(nums) {
-// Count the ones, in order to find minimum window length necessart to hold them all
+// Count the ones, in order to find minimum window length necessary to hold them all
+// Return zero early if the array has only ones, or only one zero
+    
     let oneCount = 0
     for (let num of nums) {
         if (num === 1) oneCount++
     }
-    if (oneCount === nums.length) return 0
+    if (oneCount >= nums.length - 1) return 0
+    
 // Calculate number of zeroes in initial window of size oneCount
+// Return zero early if the initial window contains all the ones
+    
     let zeroCount = 0
     for (let i = 0; i < oneCount; i++) {
         if (nums[i] === 0) zeroCount++
     }
     if (zeroCount === 0) return 0
-// Iterate through nums by sliding the window until 'start' reaches the last value
-// Track minimum number of zeroes in each window
+    
+// Iterate through nums by sliding the window [start...end] until 'start' reaches the last item in 'nums'. Calculate next 'end' value with modulus so it loops to start of circular array when necessary
+// Track minimum number of zeroes in each window by monitoring which values enter and exit the window with each window movement. Upate minZero accordingly. 
+// Return zero early if we find a window which contains all the ones
+    
     let end = oneCount-1, minZero = zeroCount
     for (let start = 0; start < nums.length; start++) {
         if (nums[start] === 0) zeroCount--
