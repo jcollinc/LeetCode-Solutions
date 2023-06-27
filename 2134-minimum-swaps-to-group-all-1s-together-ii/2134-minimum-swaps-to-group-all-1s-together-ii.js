d369da006ce0bd2124e6231c2ce1d761ecc7f52f
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-06-27 09:35:02 -0400

    Time: 105 ms (15.00%), Space: 51.3 MB (65.00%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.js
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.js
@@ -3,24 +3,24 @@
  * @return {number}
  */
 var minSwaps = function(nums) {
-    let oneCount = 0, zeroCount = 0, n = nums.length
+// Count the ones, in order to find minimum window length necessart to hold them all
+    let oneCount = 0
     for (let num of nums) {
         if (num === 1) oneCount++
     }
-    if (oneCount === n) return 0
+// Calculate number of zeroes in initial window of size oneCount
+    let zeroCount = 0
     for (let i = 0; i < oneCount; i++) {
         if (nums[i] === 0) zeroCount++
     }
-
-    let start = 0, end = oneCount - 1, minZeros = zeroCount
-    
-    while (start < n) {
-        if (zeroCount === 0) return 0
+// Iterate through nums by sliding the window until 'start' reaches the last value
+// Track minimum number of zeroes in each window
+    let end = oneCount-1, minZero = zeroCount
+    for (let start = 0; start < nums.length; start++) {
         if (nums[start] === 0) zeroCount--
-        start++
-        end = (end + 1) % n
+        end = (end + 1) % nums.length
         if (nums[end] === 0) zeroCount++
-        minZeros = Math.min(minZeros, zeroCount)
+        minZero = Math.min(minZero, zeroCount)
     }
-    return minZeros
+    return minZero
 };
