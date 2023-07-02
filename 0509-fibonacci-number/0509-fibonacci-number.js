f7b93d43960bb40f6977558c128db7ef309a3273
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-02 11:10:48 -0400

    Time: 44 ms (98.98%), Space: 41.2 MB (96.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,6 +3,13 @@
  * @return {number}
  */
 var fib = function (n) {
-    if (n <= 1) return n
-    return fib(n-1) + fib(n-2)
+    let dp = [0,1]
+    let res = dp[n]
+    for (let i = 2; i <= n; i++) {
+        let sum = dp[0] + dp[1]
+        dp[0] = dp[1]
+        dp[1] = sum
+        res = dp[1]
+    }
+    return res
 };
