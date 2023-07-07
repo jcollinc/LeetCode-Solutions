eacec05c67f8a62d081fdc0ae7a87117f8d0ebba
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-07 18:46:27 -0400

    Time: 198 ms (28.46%), Space: 97.6 MB (23.59%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.js
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.js
@@ -12,15 +12,14 @@
  */
 var deepestLeavesSum = function(root) {
     let maxDepth = 0, sum = 0
-    function dfs(node, currDepth) {
+    const dfs = (node, currDepth) => {
         if (!node) return
-        dfs(node.left, currDepth + 1)
-        dfs(node.right, currDepth + 1) 
         if (currDepth > maxDepth) {
             sum = node.val
             maxDepth = Math.max(maxDepth, currDepth)
-        }
-        else if (currDepth === maxDepth) sum += node.val
+        } else if (currDepth === maxDepth) sum += node.val
+        dfs(node.left, currDepth + 1)
+        dfs(node.right, currDepth + 1) 
     }
     dfs(root, 0)
     return sum
