182650526a04b1c8cd3288bdf9891cffd2147484
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-08 11:30:45 -0400

    Time: 61 ms (56.01%), Space: 42.5 MB (43.19%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -4,12 +4,9 @@
  */
 var runningSum = function(nums) {
     let sum = 0
-    function recurse(array, idx) {
-        if (idx >= nums.length) return
-        nums[idx] = nums[idx] + sum
-        sum = nums[idx]
-        recurse(nums, idx + 1)
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] += sum
+        sum = nums[i]
     }
-    recurse(nums, 0)
     return nums
 };
