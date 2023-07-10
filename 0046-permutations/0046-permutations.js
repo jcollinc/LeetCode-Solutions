b3b3eada37a2d5dc1b3bbfc5e71c8284ff26abf5
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-10 14:10:22 -0400

    Time: 83 ms (13.20%), Space: 45.2 MB (22.61%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -2,27 +2,26 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(letters) {
+var permute = function(nums) {
     let res = [];
-    dfs(letters, [], Array(letters.length).fill(false), res);
+    function dfs(nums, path, used) {
+        if (path.length == nums.length) {
+            res.push([...path]);
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            // skip used nums
+            if (used[i]) continue;
+            // add letter to permutation, mark letter as used
+            path.push(nums[i]);
+            used[i] = true;
+            dfs(nums, path, used);
+            // remove letter from permutation, mark letter as unused
+            path.pop();
+            used[i] = false;
+        }
+    }
+    dfs(nums, [], Array(nums.length).fill(false));
     return res;
 }
 
-function dfs(letters, path, used, res) {
-    if (path.length == letters.length) {
-        // make a deep copy since otherwise we'd be append the same list over and over
-        res.push([...path]);
-        return;
-    }
-    for (let i = 0; i < letters.length; i++) {
-        // skip used letters
-        if (used[i]) continue;
-        // add letter to permutation, mark letter as used
-        path.push(letters[i]);
-        used[i] = true;
-        dfs(letters, path, used, res);
-        // remove letter from permutation, mark letter as unused
-        path.pop();
-        used[i] = false;
-    }
-}
