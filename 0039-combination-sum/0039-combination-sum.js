aca175bb5e0abcfc68428281db26c01f4fdec045
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-10 14:44:26 -0400

    Time: 80 ms (51.75%), Space: 44.7 MB (93.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -6,17 +6,21 @@
 
 
 var combinationSum = function(candidates, target) {
-    
-    let output = []
-    
-    function dfs (array = [], idx = 0, sum = 0) {
-        if (sum > target) return
-        if (sum === target) output.push(array)
-        
+    let validCombos = []
+    function dfs(combo, sum, idx) {
+        if (sum > target) {
+            return
+        }else if (sum === target) {
+            validCombos.push([...combo])
+            return
+        }
         for (let i = idx; i < candidates.length; i++) {
-            dfs([...array, candidates[i]], i, sum + candidates[i])
+            combo.push(candidates[i])
+            sum += candidates[i]
+            dfs(combo, sum, i)
+            sum -= combo.pop()
         }
     }
-    dfs()
-    return output
+    dfs([], 0, 0)
+    return validCombos
 };
