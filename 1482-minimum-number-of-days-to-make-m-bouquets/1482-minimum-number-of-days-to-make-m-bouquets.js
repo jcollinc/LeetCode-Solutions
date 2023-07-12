c60450a795bade5d9e29620f95792b8e55598814
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-12 14:59:01 -0400

    Time: 116 ms (67.27%), Space: 63 MB (18.18%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} bloomDay
+ * @param {number} m
+ * @param {number} k
+ * @return {number}
+ */
+
+
+
+var minDays = function(bloomDay, m, k) {
+    
+    const canCreateAllBouquets = function(dayCount) {
+        let bouquets = 0, flowers = [...bloomDay], consec = 0
+        for (let i = 0; i < bloomDay.length; i++) {
+            if (flowers[i] <= dayCount) {
+                consec++
+                if (consec === k) {
+                    bouquets++
+                    consec = 0
+                }
+            } else consec = 0
+        }
+        return bouquets
+    }
+    
+    
+    let left = Math.min(...bloomDay), right = Math.max(...bloomDay)
+    
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2)
+        canCreateAllBouquets(mid) >= m ? right = mid : left = mid + 1
+    }
+    
+    console.log(left, right, canCreateAllBouquets(left))
+    return canCreateAllBouquets(left) >= m ? left : -1
+};
+
