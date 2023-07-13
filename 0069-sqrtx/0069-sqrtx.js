89d5b691b9dd78b505df9b5fe59e20ae31dc579b
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-13 18:58:17 -0400

    Time: 67 ms (83.13%), Space: 44.4 MB (9.86%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -4,7 +4,7 @@
  */
 var mySqrt = function(x) {
     if (x <= 1) return x
-    let left = 1, right = x / 2
+    let left = 1, right = x
     while (left < right) {
         let mid = Math.floor((left + right) / 2)
         if (mid * mid <= x && (mid + 1) * (mid + 1) > x) return mid 
