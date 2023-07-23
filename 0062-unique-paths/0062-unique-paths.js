54008702123f6fbace98ba5efd7a8e0e942eb2ec
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-23 15:46:27 -0400

    Time: 58 ms (72.56%), Space: 42.5 MB (39.48%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,19 +4,16 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let dp = []
-    for (let row = 0; row < m; row++) {
-        dp[row] = new Array(n).fill(0)
+    let lastRow = new Array(n).fill(1)
+    for (let row = 1; row < m; row++) {
+        let nextRow = new Array(n).fill(0)
         for (let col = 0; col < n; col++) {
-            if (row === 0 && col === 0) {
-                dp[row][col] = 1
-                continue
-            }
-            let top = row-1 >= 0 ? dp[row-1][col] : 0
-            let left = col-1 >= 0 ? dp[row][col-1] : 0
-            dp[row][col] = top + left
+            let top = lastRow[col]
+            let left = col-1 >= 0 ? nextRow[col-1] : 0
+            nextRow[col] = top + left
         }
+        lastRow = [...nextRow]
     }
-    console.log(dp)
-    return dp[m-1][n-1]
+    console.log(lastRow)
+    return lastRow[n-1]
 }
