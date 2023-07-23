29a2b0532e45308d3a09bcd19a824b79ff3cf4df
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-23 13:08:42 -0400

    Time: 74 ms (37.03%), Space: 44.5 MB (51.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -4,14 +4,20 @@
  */
 var minPathSum = function(grid) {
     let height = grid.length, width = grid[0].length
+    let dp = []
     for (let row = 0; row < height; row++) {
+        let newRow = []
+        dp.push(newRow)
         for (let col = 0; col < width; col++) {
-            if (row === 0 && col === 0) continue
-            let top = row - 1 >= 0 ? grid[row-1][col] : Infinity
-            let left = col - 1 >= 0 ? grid[row][col-1] : Infinity
-            grid[row][col] += Math.min(top, left)
-        }
+            if (row === 0 && col === 0) {
+                newRow.push(grid[0][0])
+                continue
+            }
+            let top = row - 1 >= 0 ? dp[row-1][col] : Infinity
+            let left = col - 1 >= 0 ? dp[row][col-1] : Infinity
+            newRow.push(grid[row][col] + Math.min(top, left))
+        } 
     }
-    return grid[height-1][width-1]
+    return dp[height-1][width-1]
 }
 
