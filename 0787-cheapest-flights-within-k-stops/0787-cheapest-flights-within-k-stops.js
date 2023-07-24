d38585f992db62d703006fb9577e4c154d93119a
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-24 18:23:26 -0400

    Time: 93 ms (61.16%), Space: 48.3 MB (47.77%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.js
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.js
@@ -7,32 +7,25 @@
  * @return {number}
  */
 
-// This is Dijkstra's Algorithm
+// Bellman Ford
 
-var findCheapestPrice = function(n, flights, src, dst, K) {
-    const adjacencyList = new Map();
-    
-    for(let [start, end, cost] of flights) {
-        if(adjacencyList.has(start)) adjacencyList.get(start).push([end, cost]);
-        else adjacencyList.set(start, [[end, cost]]);
+const findCheapestPrice = (n, flights, src, dst, k) => {
+  let dp = Array.from({ length: n + 1 }, () => Infinity);
+  dp[src] = 0;
+
+  // starting from source: visiting k + 1 verticies
+  // if possible: visit all possible path to destination
+  for (i = 0; i < k + 1; i++) {
+    const temp = [...dp]; // array for comparison
+
+    // find cheapeast price for each path
+    for (const [x, y, price] of flights) {
+      if (dp[x] === Infinity) continue;
+      temp[y] = Math.min(temp[y], dp[x] + price); // get cost from src -> dst and update
     }
-    
-    const queue = [[0, src, K+1]];
-    const visited = new Map();
-    
-    while(queue.length) {
-        queue.sort((a, b) => a[0] - b[0]);
-        
-        const [cost, city, stops] = queue.shift();
-        visited.set(city, stops);
-        
-        if(city === dst) return cost;
-        if(stops <= 0 || !adjacencyList.has(city))  continue;
-            
-        for(let [nextCity, nextCost] of adjacencyList.get(city)) {
-            if(visited.has(nextCity) && visited.get(nextCity) >= stops-1) continue;
-            queue.push([cost+nextCost, nextCity, stops-1]);
-        }
-    }
-    return -1;
+
+    dp = temp;
+  }
+
+  return dp[dst] !== Infinity ? dp[dst] : -1;
 };
