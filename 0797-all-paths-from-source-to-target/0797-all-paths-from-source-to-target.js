ef7496c07eea28bbf39261f01844b44d712d1c11
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-24 09:55:08 -0400

    Time: 105 ms (55.08%), Space: 50.8 MB (46.46%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -3,19 +3,20 @@
  * @return {number[][]}
  */
 var allPathsSourceTarget = function(graph) {
-    let visited = new Set()
     let n = graph.length
     let paths = []
-    let queue = [[graph[0],[0]]]
-    while (queue.length) {
-        let [curr, path] = queue.shift()
-        visited.add(curr)
-        for (let node of curr) {
-            if (node === n-1) paths.push([...path, node])
-            else if (!visited.has(node)) {
-                queue.push([graph[node], [...path, node]])
+    function dfs(node, neighbors, path, visited = new Set()) {
+        if (node === n-1) {
+            paths.push([...path])
+            return
+        }
+        visited.add(node)
+        for (let neighbor of neighbors) {
+            if (!visited.has(neighbor)) {
+                dfs(neighbor, graph[neighbor], [...path, neighbor], new Set(visited))
             }
         }
     }
+    dfs(0, graph[0], [0])
     return paths
 };
