3d26f3ccfd132c35d4a16b2ba229e3136784fd78
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-25 16:29:00 -0400

    Time: 76 ms (51.65%), Space: 51.5 MB (20.58%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -5,7 +5,7 @@
 
 const getDirection = (arr, idx) => {
     if (arr[idx] > arr[idx-1] && arr[idx] > arr[idx+1]) return 'peak'
-    else if (arr[idx] > arr[idx-1] && arr[idx] < arr[idx+1]) return 'increasing'
+    else if (arr[idx] > (arr[idx-1] || -Infinity) && arr[idx] < arr[idx+1]) return 'increasing'
     else return 'decreasing'
 }
 var peakIndexInMountainArray = function(arr) {
@@ -14,8 +14,8 @@ var peakIndexInMountainArray = function(arr) {
         let mid = Math.floor((right+left)/2)
         let direction = getDirection(arr, mid)
         if (direction === 'peak') return mid
-        else if (direction === 'increasing') left = mid
-        else right = mid
+        else if (direction === 'increasing') left = mid+1
+        else right = mid-1
     }
     return left
 };
