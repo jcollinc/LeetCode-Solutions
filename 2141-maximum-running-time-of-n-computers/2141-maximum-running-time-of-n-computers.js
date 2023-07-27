772722aad0941201a1cde408a96af367764ca7af
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-27 15:26:10 -0400

    Time: 102 ms (100.00%), Space: 55 MB (100.00%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @param {number[]} batteries
+ * @return {number}
+ */
+var maxRunTime = function(n, batteries) {
+    let sumPower = 0;
+    for (let power of batteries) {
+        sumPower += power;
+    }
+    let left = 1, right = Math.floor(sumPower / n);
+    while (left < right) {
+        let mid = Math.floor((left + right + 1) / 2);
+        if (canPowerForMidHours(mid, n, batteries)) {
+            left = mid
+        } else {
+            right = mid-1
+        }
+    }
+    return left
+}
+
+const canPowerForMidHours = (hours, n, batteries) => {
+    // Initialize the sum of the minimum of each battery and the time
+    let sum = 0;
+    for (let battery of batteries) {
+        sum += Math.min(battery, hours);
+    }
+    // Return true if the sum is greater than or equal to the product of the time and n
+    return (sum >= hours * n);
+}
+
+
