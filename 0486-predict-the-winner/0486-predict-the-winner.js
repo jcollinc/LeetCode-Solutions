6b19ca0614f99787ce43cdc79f8a27048e47e75e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-28 10:12:02 -0400

    Time: 49 ms (97.62%), Space: 42 MB (100.00%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const PredictTheWinner = (nums) => {
+    let n = nums.length
+    let memo = new Array(n).fill(-1).map(() => new Array(n).fill(-1));
+    let scoreFirst = PredictTheWinnerInSituation(nums, 0, n - 1, memo)
+    let scoreTotal = nums.reduce((a,b)=>a+b)
+    return scoreFirst >= scoreTotal - scoreFirst
+}
+
+const PredictTheWinnerInSituation = (nums, i, j, memo) => {
+    if (i > j) return 0
+    if (i === j) return nums[i]
+    if (memo[i][j] != -1) return memo[i][j]
+    
+    let curScore = Math.max(
+        nums[i] + Math.min(
+            PredictTheWinnerInSituation(nums, i+2, j, memo), 
+            PredictTheWinnerInSituation(nums, i+1, j-1, memo)
+        ), nums[j] + Math.min(
+            PredictTheWinnerInSituation(nums, i, j-2, memo), 
+            PredictTheWinnerInSituation(nums, i+1, j-1, memo))
+    )
+    memo[i][j] = curScore        
+    return curScore 
+}
