0ec21a7aa1bee2c545d76d00b54536be60a131f1
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-07-28 17:41:25 -0400

    Time: 78 ms (82.86%), Space: 43.7 MB (69.43%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -6,22 +6,16 @@
 var threeSumClosest = function(nums, target) {
     let smallestDiff = Infinity, closestSum = 0
     nums.sort((a,b)=>a-b)
-    for (let i = 0; i < nums.length; i++) {
-        let left = 0, right = nums.length-1
-        if (i===0) left++
-        if (i===nums.length-1) right--
+    for (let i = 0; i < nums.length && smallestDiff !== 0; i++) {
+        let left = i+1, right = nums.length-1
         while (left < right) {
             let sum = nums[i]+nums[left]+nums[right]
             if (Math.abs(target-sum) < smallestDiff) {
                 closestSum = sum
                 smallestDiff = Math.abs(target-sum)
             }
-            if (sum === target) return target
-            else if (sum < target) {
-                left = i === left+1 ? left+2 : left+1
-            } else {
-                right = i === right-1 ? right-2 : right-1
-            }
+            if (sum < target) left++
+            else right--
         }
     }
     return closestSum
