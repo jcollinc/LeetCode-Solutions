9a042855216920289e7ba7d26f40fe66b4cbecb4
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-01 11:58:09 -0400

    Time: 2432 ms (9.02%), Space: 0B (100.00%) - LeetHub

---
+++ 1350-students-with-invalid-departments/1350-students-with-invalid-departments.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT students.id, students.name
+FROM students 
+LEFT JOIN departments 
+ON students.department_id = departments.id 
+WHERE departments.id IS NULL
