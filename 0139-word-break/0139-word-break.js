8fd435b1ab15a4a21c9e4039d6614a329727e622
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-04 15:15:39 -0400

    Time: 57 ms (88.36%), Space: 43.6 MB (63.32%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -5,15 +5,12 @@
  */
 var wordBreak = function(s, wordDict) {
     let dp = new Array(s.length+1).fill(false)
-    dp[0] = true
     for (let i = 0; i < s.length; i++) {
-        if (dp[i] === true) {
+        if (i === 0 || dp[i] === true) {
             for (let word of wordDict) {
-                let candidate = s.slice(i, i + word.length)
-                if (word === candidate) dp[i + word.length] = true
+                if (s.slice(i, i + word.length) === word) dp[i + word.length] = true
             }
         }
     }
-    console.log(dp)
     return dp[dp.length-1]
 };
