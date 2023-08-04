9aabe9f623444e4d39e333a89ac28ca1540a3224
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-04 15:21:33 -0400

    Time: 70 ms (40.18%), Space: 44.3 MB (26.16%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -8,14 +8,8 @@ var wordBreak = function(s, wordDict) {
     const memo = new Array(s.length);
 
     function buildWord(idx) {
-        if (idx === s.length) {
-            return true;
-        }
-
-        if (typeof memo[idx] !== 'undefined') {
-            return memo[idx];
-        }
-
+        if (idx === s.length) return true;
+        if (typeof memo[idx] !== 'undefined') return memo[idx];
         for (let end = idx + 1; end <= s.length; end++) {
             const word = s.substring(idx, end);
             if (wordSet.has(word) && buildWord(end)) {
@@ -23,10 +17,8 @@ var wordBreak = function(s, wordDict) {
                 return true;
             }
         }
-
         memo[idx] = false;
         return false;
     }
-
     return buildWord(0);
 };
