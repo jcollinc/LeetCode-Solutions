c636b73b234f3305dd7d6947bd0b5b8acdc8c2fc
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-07 17:36:10 -0400

    Time: 48 ms (90.68%), Space: 42 MB (43.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var rob = function(nums) {
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+    
     let n = nums.length
     let dp = new Array(n)
-    dp[0] = nums[0]
-    if (nums[1] !== undefined) dp[1] = nums[1]
+    dp[0] = nums[0], dp[1] = nums[1]
     for (let i = 2; i < n; i++) {
         dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]-nums[i-1]+nums[i])
     }
-    console.log(dp)
-    return dp[n-2] ? Math.max(dp[n-1], dp[n-2]) : dp[n-1]
+    return Math.max(dp[n-1], dp[n-2])
 };
