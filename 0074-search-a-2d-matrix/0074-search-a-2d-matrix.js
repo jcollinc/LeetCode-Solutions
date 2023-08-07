d234d8e8cfafad48caa405609f0d28a76c3cfe03
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-06 20:27:09 -0400

    Time: 60 ms (48.81%), Space: 43.6 MB (9.33%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,12 +4,10 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let left = 0, right = (matrix.length * matrix[0].length) - 1
-    while (left <= right) {
-        let mid = Math.floor((right + left) / 2)
-        let mid1 = Math.floor(mid/matrix[0].length), mid2 = mid%matrix[0].length
-        if (matrix[mid1][mid2] === target) return true
-        else matrix[mid1][mid2] > target ? right = mid-1 : left = mid+1
+    for (let row of matrix) {
+        for (let val of row) {
+            if (val === target) return true
+        }
     }
     return false
 };
