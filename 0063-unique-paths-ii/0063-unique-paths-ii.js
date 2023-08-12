5a6828c0eb0265f017f7875c403b5186396a1113
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-12 14:07:50 -0400

    Time: 59 ms (67.99%), Space: 42.4 MB (71.82%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.js
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -3,21 +3,24 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function(obstacleGrid) {
-    if (obstacleGrid[0][0] === 1) return 0
-    let n = obstacleGrid.length, m = obstacleGrid[0].length
-    let dp = new Array(n)
-    for (let i = 0; i < n; i++) {
-        dp[i] = new Array(m)
+    if (!obstacleGrid.length || !obstacleGrid[0].length || obstacleGrid[0][0] === 1) {
+        return 0;
     }
-    dp[0][0] = 1
-    for (let row = 0; row < n; row++) {
-        for (let col = 0; col < m; col++) {
-            if (row === 0 && col === 0) continue
-            else if (obstacleGrid[row][col] === 1) dp[row][col] = 0
-            else if (row === 0) dp[row][col] = dp[row][col-1] ? 1 : 0  
-            else if (col === 0) dp[row][col] = dp[row-1][col] ? 1 : 0
-            else dp[row][col] = dp[row-1][col] + dp[row][col-1]
+
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+
+    let previous = new Array(n).fill(0);
+    let current = new Array(n).fill(0);
+    previous[0] = 1;
+
+    for (let i = 0; i < m; i++) {
+        current[0] = obstacleGrid[i][0] === 1 ? 0 : previous[0];
+        for (let j = 1; j < n; j++) {
+            current[j] = obstacleGrid[i][j] === 1 ? 0 : current[j-1] + previous[j];
         }
+        previous = [...current];
     }
-    return dp[n-1][m-1]
+
+    return previous[n-1];
 };
