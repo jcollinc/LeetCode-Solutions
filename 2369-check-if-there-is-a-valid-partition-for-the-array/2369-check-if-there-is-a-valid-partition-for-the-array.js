0c9d1e6eaa489f149c4b87227e3547593d612a5b
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-13 14:14:29 -0400

    Time: 86 ms (85.71%), Space: 54.9 MB (100.00%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.js
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.js
@@ -2,28 +2,21 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var validPartition = function(nums) {
-    let n = nums.length;
-
-    if (n === 1) return false;
-
-    let dp = [true, false, n > 1 && nums[0] === nums[1]];
-
-    for (let i = 2; i < n; i++) {
-        let current_dp = false;
-
-        if (nums[i] === nums[i-1] && dp[1]) {
-            current_dp = true;
-        }
-        else if (nums[i] === nums[i-1] && nums[i] === nums[i-2] && dp[0]) {
-            current_dp = true;
-        }
-        else if (nums[i] - nums[i-1] === 1 && nums[i-1] - nums[i-2] === 1 && dp[0]) {
-            current_dp = true;
-        }
-
-        [dp[0], dp[1], dp[2]] = [dp[1], dp[2], current_dp];
+const validPartition = (nums) => {
+    let n = nums.length
+    let dp = Array(n + 1).fill(false);
+    dp[0] = true;
+    for (let i = 2; i <= n; i++) {
+        if (dp[i - 2] && nums[i - 2] == nums[i - 1]) dp[i] = true; 
+        if (i-3 >= 0 && dp[i-3] 
+            && nums[i-1] == nums[i-2] 
+            && nums[i-2] == nums[i-3]
+        ) dp[i] = true; 
+        if (i - 3 >= 0 
+            && dp[i-3] 
+            && nums[i-1] - nums[i-2] == 1 
+            && nums[i-2] - nums[i-3] == 1
+        ) dp[i] = true; 
     }
-
-    return dp[2];
+    return dp[n];
 };
