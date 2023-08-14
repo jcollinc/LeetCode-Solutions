a584e30453097aa79786fb622f0571d0d700d9c7
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-14 12:17:36 -0400

    Time: 97 ms (85.71%), Space: 55.1 MB (85.71%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.js
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.js
@@ -6,12 +6,23 @@ const validPartition = (nums) => {
     let n = nums.length
     let dp = Array(n + 1).fill(false);
     dp[0] = true;
+    
     for (let i = 2; i <= n; i++) {
+        
+        // if condition 1 satisfied
+        
         if (dp[i - 2] && nums[i - 2] == nums[i - 1]) dp[i] = true; 
-        if (i-3 >= 0 && dp[i-3] 
+        
+        // if condition 2 satisfied
+        
+        if (i-3 >= 0 
+            && dp[i-3] 
             && nums[i-1] == nums[i-2] 
             && nums[i-2] == nums[i-3]
         ) dp[i] = true; 
+        
+        //if condition 3 satisfied
+        
         if (i - 3 >= 0 
             && dp[i-3] 
             && nums[i-1] - nums[i-2] == 1 
