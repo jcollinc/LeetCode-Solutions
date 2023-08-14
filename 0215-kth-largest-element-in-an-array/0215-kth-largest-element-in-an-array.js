0f59cc07f758a6819f601cc3dd3ca1d9740786a5
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-14 12:29:06 -0400

    Time: 138 ms (64.33%), Space: 51 MB (67.98%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,16 +4,5 @@
  * @return {number}
  */
 var findKthLargest = function (nums, k) { 
-	let minHeap = new MinPriorityQueue(k);
-	for (let num of nums) {
-		if (minHeap.size() < k) {
-			minHeap.enqueue(num);
-		} else {
-			if (minHeap.front().element < num) {
-				minHeap.dequeue();
-				minHeap.enqueue(num);
-			}
-		}
-	}
-	return minHeap.dequeue().element;
+    return nums.sort((a,b) => b-a)[k-1]
 };
