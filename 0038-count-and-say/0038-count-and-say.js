d95dd7a4cd67a7f987ab94da63c890790aae35b0
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-15 17:10:41 -0400

    Time: 67 ms (46.80%), Space: 43.5 MB (96.89%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.js
+++ 0038-count-and-say/0038-count-and-say.js
@@ -5,19 +5,19 @@
 var countAndSay = function(n) {
     let curr = '1'
     for (let i = 1; i < n; i++) {
-        curr = convertToStr(curr)
+        curr = getNextSequence(curr)
     }
     return curr
 };
 
-function convertToStr (str) {
-    let idx = 0, nextConversion = ""
+function getNextSequence (str) {
+    let idx = 0, nextSequence = ""
     while (idx < str.length) {
         let count = 0, curr = str[idx]
         while (idx < str.length && str[idx] === curr) {
             idx++, count++
         }
-        nextConversion = nextConversion + count + curr
+        nextSequence = nextSequence + count + curr
     }
-    return nextConversion 
+    return nextSequence 
 }
