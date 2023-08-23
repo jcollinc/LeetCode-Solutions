ffe5247a41d3626b0b3bc463c25d7736cfa3f12e
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-23 09:05:15 -0400

    Time: 56 ms (92.13%), Space: 44.4 MB (68.50%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.js
+++ 0767-reorganize-string/0767-reorganize-string.js
@@ -2,38 +2,23 @@
  * @param {string} s
  * @return {string}
  */
-var reorganizeString = function(s) {
-    let charCount = new Map();
-    for (let char of s) {
-        charCount.set(char, (charCount.get(char) || 0) + 1);
-    }
-    
-    let maxPriorityQueue = new MaxPriorityQueue(), 
-          result = []
-    
-    for (let [char, count] of charCount) {
-        maxPriorityQueue.enqueue({ char, count }, count);
-    }
-    
-    while (!maxPriorityQueue.isEmpty()) {
-        let curr = maxPriorityQueue.dequeue().element;
-        if (result.length === 0 || curr.char !== result[result.length - 1]) {
-            result.push(curr.char);
-            if (--curr.count > 0) {
-                maxPriorityQueue.enqueue(curr, curr.count);
-            }
-        } else {
-            let next = maxPriorityQueue.dequeue();
-            if (next) next = next.element
-            else return ""
-            result.push(next.char);
-            if (--next.count > 0) {
-                maxPriorityQueue.enqueue(next, next.count);
-            }
-            maxPriorityQueue.enqueue(curr, curr.count);
-        }
-    }
-    return result.join("");
-};
+var reorganizeString = function(S) {
+    let hash = {};
+    for (let c of S) hash[c] = hash[c] + 1 || 1;
 
+    let sort = Object.keys(hash).sort((a,b)=>hash[b] - hash[a]);
+    let res = [];
+    let index = 0;
+
+    for (let i = 0;i<sort.length;i++) {
+        let occur = hash[sort[i]];
+        if (occur > parseInt((S.length + 1)/2)) return "";
+        for (let j = 0;j < occur;j++) {
+            if (index >= S.length) index = 1;
+            res[index] = sort[i];
+            index += 2;
+        }
+    } 
+    return res.join('');
+};
 
