6e42f06d6441de0951b298b7acbba78a7af41e65
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-23 22:17:18 -0400

    Time: 50 ms (77.78%), Space: 41.6 MB (81.94%) - LeetHub

--- 0068-text-justification/0068-text-justification.js
+++ 0068-text-justification/0068-text-justification.js
@@ -3,60 +3,23 @@
  * @param {number} maxWidth
  * @return {string[]}
  */
-var fullJustify = function(words, maxWidth) {
-    let lineMap = [];
-    let line = 0, charsInLine = 0, lastLine = 0;
+const fullJustify = (words = [], maxWidth) => {
+  let ans = [];
+  let row = [];
+  let rowLetters = 0;
 
-    for (let i = 0; i < words.length; i++) {
-        let wordLen = words[i].length;
-        
-        if (charsInLine + wordLen + (i - lastLine) > maxWidth) {
-            line++;
-            lastLine = i;
-            charsInLine = wordLen;
-            lineMap.push([words[i]]);
-        } else {
-            charsInLine += wordLen;
-            if (lineMap[line]) {
-                lineMap[line].push(words[i]);
-            } else {
-                lineMap[line] = [words[i]];
-            }
-        }
+  for (let word of words) {
+    if (rowLetters + word.length + row.length > maxWidth) {
+      for (let i = 0; i < maxWidth - rowLetters; i++)
+        row[i % (row.length - 1 || 1)] += " ";
+      ans.push(row.join(""));
+      row = [];
+      rowLetters = 0;
     }
-
-    let res = [];
-    for (let i = 0; i < lineMap.length; i++) {
-        let words = lineMap[i];
-        let str = words.join(' ');
-
-        if (i !== lineMap.length - 1) {
-            let len = words.reduce((acc, word) => acc + word.length, 0);
-            let spaces = maxWidth - len;
-
-            if (words.length > 1) {
-                let slots = words.length - 1;
-                let spacesBetweenWords = Math.floor(spaces / slots);
-                let extraSpaces = spaces % slots;
-                str = words.reduce((acc, word, idx) => {
-                    if (idx === words.length - 1) {
-                        return acc + word;
-                    }
-                    let spacesToAdd = spacesBetweenWords + (extraSpaces > 0 ? 1 : 0);
-                    extraSpaces--;
-                    return acc + word + ''.padStart(spacesToAdd);
-                }, '');
-            } else {
-                str += ''.padStart(spaces);
-            }
-        } else {
-            str += ''.padStart(maxWidth - str.length);
-        }
-
-        res.push(str);
-    }
-    
-    return res;
+    row.push(word);
+    rowLetters += word.length;
+  }
+  return ans.concat(row.join(" ").padEnd(maxWidth));
 };
 
 
