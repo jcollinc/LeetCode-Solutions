361f166c96c0aa56c44ee0ec62a3e55b4bc88d91
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-08-26 09:14:58 -0400

    Time: 70 ms (87.74%), Space: 46 MB (63.21%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.js
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.js
@@ -3,19 +3,13 @@
  * @return {number}
  */
 var findLongestChain = function(pairs) {
-    if (pairs.length === 0) {
-        return 0;
-    }
-    pairs.sort((a, b) => a[0] - b[0]);
-    const dp = new Array(pairs.length).fill(1);
-    let maxLen = 1;
-    for (let i = 1; i < pairs.length; i++) {
-        for (let j = 0; j < i; j++) {
-            if (pairs[j][1] < pairs[i][0]) {
-                dp[i] = Math.max(dp[i], dp[j] + 1);
-                maxLen = Math.max(maxLen, dp[i]);
-            }
+    pairs.sort((a, b) => a[1] - b[1]);
+    let cur = -Infinity, ans = 0;
+    for (const [start, end] of pairs) {
+        if (cur < start) {
+            cur = end;
+            ans++;
         }
     }
-    return maxLen;
+    return ans;
 };
