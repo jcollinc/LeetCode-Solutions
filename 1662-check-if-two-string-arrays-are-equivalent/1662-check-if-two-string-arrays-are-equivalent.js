15c94764d0118569ddb26f4655ae1b69b72c5c37
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2023-12-01 12:56:42 -0500

    Time: 66 ms (5.02%), Space: 42.5 MB (9.31%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -4,5 +4,15 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function(word1, word2) {
-    return word1.join("") === word2.join("")
+    let [pword1, pword2, pchar1, pchar2] = [0,0,0,0]
+    while (pword1 < word1.length && pword2 < word2.length) {
+        if (word1[pword1][pchar1] !== word2[pword2][pchar2]) return false
+        else pchar1++, pchar2++
+        if (pchar1 >= word1[pword1].length) {
+            pword1++, pchar1 = 0
+        } if (pchar2 >= word2[pword2].length) {
+            pword2++, pchar2 = 0
+        }
+    }
+    return pword1 === word1.length && pword2 === word2.length
 };
