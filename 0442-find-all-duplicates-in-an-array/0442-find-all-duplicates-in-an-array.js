fc0cf70e59ad35e9b2ef88dced4e1154882502a7
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2024-03-25 11:34:03 -0400

    Time: 86 ms (60.34%), Space: 60.4 MB (50.47%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -3,10 +3,9 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-    let countMap = new Map()
-    let res = []
+    let numSet = new Set(), res = []
     for (let num of nums) {
-        countMap.has(num) ? res.push(num) : countMap.set(num, 1)
+        numSet.has(num) ? res.push(num) : numSet.add(num)
     }
     return res
 };
