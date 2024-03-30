945fac5527bb28c96fd8ac0146b7266a04da9c52
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2024-03-29 22:41:39 -0400

    Time: 97 ms (48.00%), Space: 57.9 MB (18.00%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.js
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.js
@@ -1,14 +1,15 @@
 function subarraysWithKDistinct(nums, k) {
     function atMostKDistinct(k) {
-        let count = 0;
-        let left = 0;
-        const map = new Map();
+        let count = 0, left = 0, map = new Map();
         for (let right = 0; right < nums.length; right++) {
-            if (!map.has(nums[right]) || map.get(nums[right]) === 0) k--
+            if (!map.has(nums[right])) k--;
             map.set(nums[right], (map.get(nums[right]) || 0) + 1);
             while (k < 0) {
                 map.set(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) === 0) k++;
+                if (map.get(nums[left]) === 0) {
+                    map.delete(nums[left]);
+                    k++;
+                }
                 left++;
             }
             count += right - left + 1;
@@ -17,4 +18,3 @@ function subarraysWithKDistinct(nums, k) {
     }
     return atMostKDistinct(k) - atMostKDistinct(k - 1);
 }
-
