44c6bbd817ebafcfd37aea5daa3d68d491b92100
Jon Chinje <92186183+jcollinc@users.noreply.github.com>
2024-11-18 15:19:59 -0500

    Time: 1 ms (72.73%), Space: 50.1 MB (64.93%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.js
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.js
@@ -4,23 +4,34 @@
  * @return {number[]}
  */
 var decrypt = function(code, k) {
-    let n = code.length, decrypt = new Array(n)
-    if (k == 0) return decrypt.fill(0)
-    for (let i = 0; i < code.length; i++) {
-        let count = k, idx = i, curr = 0
-        if (k > 0) {
-            while (count > 0) {
-                curr += code[(idx+1) % n]
-                count--, idx++
-            }
-            decrypt[i] = curr
-        } else if (k < 0) {
-            while (count < 0) {
-                curr += code[(idx-1+n) % n]
-                count++, idx--
-            }
-        } 
-        decrypt[i] = curr
+    let n = code.length, decrypt = []
+    
+    if (k == 0) return new Array(n).fill(0)
+    
+    if (k > 0) {
+        let count = k, idx = 0, sum = 0
+        while (count > 0) {
+            sum += code[(idx+1) % n]
+            count--, idx++
+        }
+        decrypt.push(sum)
+        for (let p1 = 1, p2 = k; p1 != 0; p1 = (p1+1) % n, p2++) {
+            sum = (sum - code[p1] + code[(p2+1) % n])
+            decrypt.push(sum)
+        }
+    }
+    
+    else if (k < 0) {
+        let count = k, idx = 0, sum = 0
+        while (count < 0) {
+            sum += code[(idx-1+n) % n]
+            count++, idx--
+        }
+        decrypt.push(sum)
+        for (let p1 = n+k, p2 = 0; p1 != n+k-1; p1 = (p1+1) % n, p2 = (p2+1) % n) {
+            sum = (sum - code[p1] + code[p2])
+            decrypt.push(sum)
+        }
     }
     return decrypt
 };
